03a9520f9620fcb63577625542932205b4d1ef92
Mike DeAngelis <mikedeangelis@MikeMacbookPro.local>
2023-11-01 22:03:25 -0400

    Add dark/light mode

--- app/blog/add-cloudwatch-rum-to-nextjs-app/page.tsx
+++ app/blog/add-cloudwatch-rum-to-nextjs-app/page.tsx
@@ -2,63 +2,67 @@ import Link from "next/link";
 
 export default function RUMNExtJsPage() {
   return (
-    <div className='pageContainer'>
-      <h1>Add CloudWatch RUM to a Next.js app</h1>
-      <p>
-        This tutorial will assume that you&apos;ve already set up your
-        CloudWatch RUM monitor in AWS CloudWatch. If you haven&apos;t follow
-        those instructions{" "}
-        <a
-          href='https://github.com/aws-observability/aws-rum-web#readme'
-          target='_blank'
-        >
-          here
-        </a>
-        .
-      </p>
-      <hr />
-      <p>
-        CloudWatch RUM (Real User Monitoring) is a powerful tool within the AWS
-        suite that allows you to collect client-side metrics and performance
-        data.
-      </p>
-      <p>
-        There is enough documentation available on how to add RUM to any React
-        application, but if you are using Next.js there are some extra steps in
-        order to get up and running.
-      </p>
-      <p>
-        The{" "}
-        <a
-          href='https://github.com/aws-observability/aws-rum-web#readme'
-          target='_blank'
-        >
-          RUM web client documentation
-        </a>{" "}
-        suggests using the HTML snippet, but other than getting some IDE errors
-        when pasting it in, since we&apos;re (probably) using Typescript for our
-        Next.js app, it makes sense to use Typescript to initialize RUM.
-      </p>
-      <p>
-        When we insert the RUM code in our layout.tsx file (or whereever you
-        decide to place the initilzation code), we&apos;ll see the following
-        error in our console.
-      </p>
-      <code>ReferenceError: window is not defined</code>
-      <p>
-        This is because by default, Next.js pages are rendered server side and
-        RUM is trying to access the window object, which is only available
-        client-side.
-      </p>
-      <p>
-        To fix this, we can set our Next.js layout.tsx file to be rendered
-        client side by inserting the following at the top of the file:
-      </p>
-      <code>&quot;use client&quot;;</code>
-      <p>
-        This tells Next.js to render it as a client-side component. Now when we
-        run out RUM initialization code, we&apos;re error free!
-      </p>
+    <div className='pageContainer blog'>
+      <article>
+        <span>October 23, 2023</span>
+        <h1>Add CloudWatch RUM to a Next.js app</h1>
+        <p>
+          This tutorial will assume that you&apos;ve already set up your
+          CloudWatch RUM monitor in AWS CloudWatch. If you haven&apos;t follow
+          those instructions{" "}
+          <a
+            href='https://github.com/aws-observability/aws-rum-web#readme'
+            target='_blank'
+          >
+            here
+          </a>
+          .
+        </p>
+        <hr />
+        <p>
+          CloudWatch RUM (Real User Monitoring) is a powerful tool within the
+          AWS suite that allows you to collect client-side metrics and
+          performance data.
+        </p>
+        <p>
+          There is enough documentation available on how to add RUM to any React
+          application, but if you are using Next.js there are some extra steps
+          in order to get up and running.
+        </p>
+        <p>
+          The{" "}
+          <a
+            href='https://github.com/aws-observability/aws-rum-web#readme'
+            target='_blank'
+          >
+            RUM web client documentation
+          </a>{" "}
+          suggests using the HTML snippet, but other than getting some IDE
+          errors when pasting it in, since we&apos;re (probably) using
+          Typescript for our Next.js app, it makes sense to use Typescript to
+          initialize RUM.
+        </p>
+        <p>
+          When we insert the RUM code in our layout.tsx file (or whereever you
+          decide to place the initilzation code), we&apos;ll see the following
+          error in our console.
+        </p>
+        <code>ReferenceError: window is not defined</code>
+        <p>
+          This is because by default, Next.js pages are rendered server side and
+          RUM is trying to access the window object, which is only available
+          client-side.
+        </p>
+        <p>
+          To fix this, we can set our Next.js layout.tsx file to be rendered
+          client side by inserting the following at the top of the file:
+        </p>
+        <code>&quot;use client&quot;;</code>
+        <p>
+          This tells Next.js to render it as a client-side component. Now when
+          we run out RUM initialization code, we&apos;re error free!
+        </p>
+      </article>
     </div>
   );
 }
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,21 +1,67 @@
+import * as React from "react";
 import Link from "next/link";
 import styles from "/styles/navbar.module.css";
 
-export const Navbar = () => (
-  <>
-    <nav className={styles.nav}>
-      <div className={styles.navLeft}>
-        <a href='/' className={styles.logo}>
-          m<span>ike</span>/d<span>eangelis</span>
-        </a>
-      </div>
-      <div className={styles.navRight}>
-        <Link href='/about'>about</Link>
-        <Link href='/blog'>blog</Link>
-        <Link href='/projects'>projects</Link>
-        <Link href='/uses'>uses</Link>
-        <Link href='/contact'>contact</Link>
-      </div>
-    </nav>
-  </>
-);
+export const Navbar = () => {
+  const [isDarkMode, setIsDarkMode] = React.useState(false);
+
+  function handleThemeToggle(e: any) {
+    setIsDarkMode(!isDarkMode);
+    if (isDarkMode) {
+      document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+    }
+  }
+
+  return (
+    <>
+      <nav className={styles.nav}>
+        <div className={styles.navLeft}>
+          <a href='/' className={styles.logo}>
+            m<span>ike</span>/d<span>eangelis</span>
+          </a>
+        </div>
+        <div className={styles.navRight}>
+          <Link href='/about'>about</Link>
+          <Link href='/blog'>blog</Link>
+          <Link href='/projects'>projects</Link>
+          <Link href='/uses'>uses</Link>
+          <Link href='/contact'>contact</Link>
+          <div className='themeToggle' onClick={handleThemeToggle}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='#fff'
+              viewBox='0 0 24 24'
+              strokeWidth='1.5'
+              stroke='#fff'
+              className='w-6 h-6'
+              id='sun'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z'
+              />
+            </svg>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='#020c1b'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='#020c1b'
+              className='w-6 h-6'
+              id='moon'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z'
+              />
+            </svg>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
